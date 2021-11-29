e324bc120b95d81a3545c7971a456a91a848ee35
ngqp2k <ngqp2k@outlook.com>
2021-11-29 22:56:20 +0700

    First commit

---
+++ src/main/java/com/firstappjavafx/Test02.java
@@ -0,0 +1,39 @@
+package com.firstappjavafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Test02 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Second window");
+
+        Button button = new Button();
+        button.setText("Press");
+        button.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("pressed");
+                    }
+                }
+        );
+
+        Pane layout = new Pane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 500, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
